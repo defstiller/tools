64cb5404f2d86c71153c66ad935952ac42507f63
defstiller <81063003+defstiller@users.noreply.github.com>
2022-06-21 10:29:54 -0500

    added labels and roles

--- src/components/header/navBar/NavBar.jsx
+++ src/components/header/navBar/NavBar.jsx
@@ -2,27 +2,25 @@ import React from "react";
 import {NavLink} from "react-router-dom";
 import PropTypes from "prop-types";
 
-import classes from "./navBar.module.css";
-
 function NavBar(props) {
 	const {styles} = props;
 	return(
 		<nav>
 			<ul className={styles.NavBar}>
 				<li>
-					<NavLink to="/tools/shop">
+					<NavLink to="/tools/shop" aria-label="Page with our products" name="shopPage">
 						Shop
 					</NavLink>
 				</li>
 
 				<li>
-					<NavLink to="/tools/about">
+					<NavLink to="/tools/about" aria-label="Read more about us">
 						About
 					</NavLink>
 				</li>
 				
 				<li>
-					<NavLink to="/tools/contact">
+					<NavLink to="/tools/contact" aria-label="Need help? Contact us!">
 						Contact
 					</NavLink>
 				</li>
