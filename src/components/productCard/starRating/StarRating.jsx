3fc4737ae18052f03adfd13d28d5c6da8c7979c2
defstiller <81063003+defstiller@users.noreply.github.com>
2022-06-28 19:42:40 -0500

    updated rating for edge cases

--- src/components/productCard/starRating/StarRating.jsx
+++ src/components/productCard/starRating/StarRating.jsx
@@ -1,23 +1,37 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import PropTypes from "prop-types";
 
 import EmptyStarSvg from "../../../assets/svgsReactReady/stars/EmptyStarSvg";
 import StarSvg from "../../../assets/svgsReactReady/stars/StarSvg";
 function StarRating(props){
 	const {rating, width} = props;
-	const newArrayFilled = new Array(rating).fill(null);
-	const emptyStars = 5 - rating;
-	const newArrayEmpty = new Array(emptyStars).fill(null);
+	const [starRating, setStarRating] = useState({});
+	useEffect(() => {
+		if(rating >= 0 && rating <= 5) {
+			const newArrayFilled = rating && new Array(rating).fill(null);
+			const emptyStars = 5 - rating;
+			const newArrayEmpty = new Array(emptyStars).fill(null);
+			setStarRating({
+				newArrayFilled,
+				newArrayEmpty
+			});
+		} else { // for edge cases, if rating is > 5 or < 0 will set rating as 5
+			const newArrayFilled = new Array(5).fill(null);
+			setStarRating({
+				newArrayFilled
+			});
+		}
+	}, [rating]);
 
 	return (
 		<div>
-			{newArrayFilled.map( () => {
+			{ starRating.newArrayFilled && starRating.newArrayFilled.map( () => {
 				const key = Math.random()*100;
-				return <StarSvg key={key} width={width}/>;
+				return <StarSvg key={key} width={width} data-testid="star"/>;
 			})}
-			{newArrayEmpty.map( () => {
+			{starRating.newArrayEmpty && starRating.newArrayEmpty.map( () => {
 				const key = Math.random()*100;
-				return <EmptyStarSvg key={key} width={width}/>;
+				return <EmptyStarSvg key={key} width={width} data-testid="emptyStar"/>;
 			})}
 		</div>
 	);
