91926d0da95cf00c6fe15ba3281989c80229b7b2
defstiller <81063003+defstiller@users.noreply.github.com>
2022-07-08 13:58:03 -0500

    added header

--- src/pages/productPage/ProductPageLayout.jsx
+++ src/pages/productPage/ProductPageLayout.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 import ProductInfo from "./productInfo/ProductInfo";
-
+import HeaderLayout from "../../components/header/HeaderLayout";
 function ProductInfoPageLayout() {
 
-	return (
+	return <>
+		<HeaderLayout />
 		<ProductInfo />
-	);
+	</>;
 }
 
 export default ProductInfoPageLayout;
