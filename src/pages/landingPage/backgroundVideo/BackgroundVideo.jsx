330fdfeffb4edb2641be3b4685801597b3c950b2
defstiller <81063003+defstiller@users.noreply.github.com>
2022-07-15 02:05:36 -0500

    BackgroundVideo is reworked
    Pretty much complete rework, now video depends on user screen size, they are hosted on video hosting, attempted to resolve issues with slow 3g connection, but so far onCanPlayThrough is the best option

--- src/pages/landingPage/backgroundVideo/BackgroundVideo.jsx
+++ src/pages/landingPage/backgroundVideo/BackgroundVideo.jsx
@@ -1,15 +1,47 @@
-import React from "react";
-
-import backgroundVideo from "../../../assets/videos/backgroundVideos/mechanic.webm";
+import React, {useState, useEffect, useRef} from "react";
 import classes from "./backgroundVideo.module.css";
-
 function LandingPageVideo() {
+	const [source, setSource] = useState();
+	const videoRef = useRef();
+	useEffect(() => {
+		const width = window.innerWidth; // i did not use window resize context because i do not want otside factors to cause video to reload
+		if(width >= 800) {
+			setSource("https://s3.tebi.io/reacttest/mechanicFullHd.webm");
+		} else {
+			setSource("https://s3.tebi.io/reacttest/mechanicHd.webm");
+		}
+	}, []);
+	function handleVideoPlay() {
+		const video = videoRef.current;
+		if(video.currentTime === 0) {
+			video.play();
+		}
+		return;
+	}
+	if(source) {
 
-	return(
-		<video autoPlay loop muted className={classes.backgroundVid}>
-			<source src={backgroundVideo} />
-		</video>
-	);
+		return(
+			<video
+				playsInline
+				loop 
+				muted 
+				className={ classes.backgroundVid}
+				width="100%" 
+				height="100%"
+				ref={videoRef}
+				onCanPlayThrough={() => handleVideoPlay()}
+				poster={require("./mechanic_Moment.webp")}
+
+			>
+				<source 
+					src={source}
+					type="video/webm"
+				/>
+				Your browser does not support the video tag.
+			</video>
+		);
+	}
+	return null;
 }
 
 export default LandingPageVideo;
