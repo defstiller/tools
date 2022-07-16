3d5ef8fa0070a98bd6ba2ac970cf16e69ca5e9be
defstiller <81063003+defstiller@users.noreply.github.com>
2022-07-16 16:07:10 -0500

    Home page css reworked
    Reworked css and jsx files to properly adjust to different screens

--- src/pages/landingPage/LandingPageLayout.jsx
+++ src/pages/landingPage/LandingPageLayout.jsx
@@ -4,15 +4,13 @@ import HeaderLayout from "../../components/header/HeaderLayout";
 import BackgroundVideo from "./backgroundVideo/BackgroundVideo";
 import Tagline from "./tagline/Tagline";
 import ShopNowButton from "./shopNowButton/ShopNowButton";
-
 import Loading from "../../assets/svgsReactReady/loading/LoadingSvg";
-
 function LandingPageLayout() {
 	return (<>
-			<HeaderLayout />
-			<BackgroundVideo />
-			<Tagline />
-			<ShopNowButton />
+		<HeaderLayout />
+		<BackgroundVideo />
+		<Tagline/>
+		<ShopNowButton/>
 	</>
 	);
 }
